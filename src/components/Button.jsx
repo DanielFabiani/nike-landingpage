c940c81613a46aae7dd8f18c072fdf39f071ffa2
danielfabiani.dev <danielfabiani.dev@gmail.com>
2023-11-07 17:43:53 -0300

    section suscribe terminada

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,16 +4,18 @@ const Button = ({
   backgroundColor,
   borderColor,
   textColor,
+  fullWidth,
 }) => {
   return (
     <button
       className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
-    ${
-      backgroundColor
-        ? `${backgroundColor} ${borderColor} ${textColor}`
-        : "bg-coral-red  text-white border-coral-red"
-    }
-    `}
+        ${
+          backgroundColor
+            ? `${backgroundColor} ${borderColor} ${textColor}`
+            : "bg-coral-red  text-white border-coral-red"
+        }
+        ${fullWidth && "w-full"}
+      `}
     >
       {label}
       {iconURL && (
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,4 +1,31 @@
+import Button from "../components/Button";
+
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      className="max-container flex justify-between items-center max-lg:flex-col gap-10"
+      id="contact-us"
+    >
+      <h3 className="text-3xl leading-11 lg:max-w-md font-palanquin font-bold">
+        Suscríbete para recibir
+        <span className="text-coral-red "> Actualizaciones </span>& nuestro
+        Newsletter
+      </h3>
+
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+        <input
+          type="text"
+          placeholder="name@example.com"
+          className="input"
+        />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button
+            label="Suscríbete"
+            fullWidth
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Subscribe;
